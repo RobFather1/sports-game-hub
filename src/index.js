ff44af77aed2ec5c34e906b40c5588c1552b5f97
Rob Giganti <79335137+RobFather1@users.noreply.github.com>
2026-02-02 16:26:22 -0600

    feat: add Clerk for authentication and update dependencies
    - Added @clerk/clerk-react as a dependency to enable authentication features.
    - Wrapped the App component with ClerkProvider in index.js to provide Clerk context.
    - Updated react-scripts dependency to use a caret version for potential updates.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ClerkProvider } from '@clerk/clerk-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+ <React.StrictMode>
+    <ClerkProvider publishableKey={process.env.REACT_APP_CLERK_PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>
 );
 
